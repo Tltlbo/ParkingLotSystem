1b427b9e1be28bae23893028135c22e4ef39d1c5
Tltlbo <js1436kt@gmail.com>
2026-08-26 16:08:34 +0900

    hw: ioc 핀 활성화

--- ParkingDetect/Core/Src/main.c
+++ ParkingDetect/Core/Src/main.c
@@ -18,6 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
+#include "dma.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -87,7 +89,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
